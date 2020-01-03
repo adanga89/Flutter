e34606c215b3dc0d83d38b1af6b68da1b36b8fe1
adan <cuadrado015@gmail.com>
2020-01-03 16:33:15 -0600

    carga de menu

--- componentes/lib/src/pages/home_page.dart
+++ componentes/lib/src/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:componentes/src/utils/icono_string_util.dart';
 import 'package:flutter/material.dart';
+import 'package:componentes/src/providers/menu_provider.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -12,19 +14,49 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista(){
-    return ListView(
-      children: _listaItems(),
+  //print(menuProvider.opciones);
+  //El siguiente método no es viable porque se crea la lista y después recupera los datos.
+  // menuProvider.cargarData().then((opciones) {
+  //   print( opciones );
+  // });
+  // return ListView(
+  //   children: _listaItems(),
+  // );
+
+  //Para la carga de datos es viable utilizar futureBuilder
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot){
+       return ListView(
+         children: _listaItems( snapshot.data ),
+       );
+      },
     );
   }
       
-  List<Widget> _listaItems() {
-    return [
-      ListTile( title: Text("Hola Mundo")),
-      Divider(),
-      ListTile( title: Text("Hola Mundo")),
-      Divider(),
-      ListTile( title: Text("Hola Mundo")),
-      Divider(),
-    ];
+  List<Widget> _listaItems(List<dynamic> data) {
+    
+    final List<Widget> opciones = [];
+
+    data.forEach((opt) {
+      final wtemp = ListTile( 
+        title: Text(opt['texto']),
+        //leading, asigna un widget al inicio
+        leading: getIcon( opt['icon']),
+        //trailing, asigna un widget al final del elemento
+        trailing: Icon( Icons.keyboard_arrow_right),
+        onTap: () {},
+      );
+
+      opciones..add(wtemp)
+              ..add(Divider());
+    });
+
+    return opciones;
   }
 }
+
+//Para agregar Recursos Físicos abrimos el pubspect.yaml, se descomenta la parte de asset y se agrega con un - NombreCarpeta/Archivo.extension
+
+//Convertir Json en un MAP es necesario importar 'dart:convert' y utilizar json.decode(data);
---
+++ componentes/lib/src/providers/menu_provider.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+
+class _MenuProvider {
+  
+  List<dynamic> opciones = [];
+
+  _MenuProvider(){
+    //cargarData();
+  }
+
+  Future<List<dynamic>> cargarData() async {
+
+    //importar services.dart
+    //se agrega "show" para indicar el método se require, en este caso rootBundle
+
+    final resp = await rootBundle.loadString('data/menu.json');
+    Map dataMap = json.decode(resp);
+    opciones = (dataMap['rutas']);
+
+    return opciones;
+  }
+
+}
+
+final menuProvider = new _MenuProvider();
